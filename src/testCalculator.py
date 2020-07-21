256db237ae6c54cd9024dc79b349a4e35efbd67f
Anurag <bannuranurag@gmail.com>
2020-07-21 11:56:43 -0400

    testing code coverage for + op

--- src/testCalculator.py
+++ src/testCalculator.py
@@ -16,7 +16,7 @@ class MyTest(unittest.TestCase):
     
     def test1(self):
         self.assertEqual(5, calculator("+",2,3))
-        self.assertEqual(3, calculator("-",5,2))
+        self.assertEqual(7, calculator("+",5,2))
         
     def test2(self):
         self.assertEqual(9, calculator("+",8,1))
