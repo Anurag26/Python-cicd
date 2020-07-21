f0b12d9fac446324484fd898414a98cb97e2ff00
Anurag <bannuranurag@gmail.com>
2020-07-21 11:31:10 -0400

    adding src and coverage files

---
+++ src/calculator.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 20 23:53:35 2020
+
+@author: sanur
+"""
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import unittest
+
+def calculator(operation, num1, num2):
+    if operation == "+":
+        return num1+num2
+    elif operation == "-":
+        return num1-num2
+    elif operation == "*":
+        return num1*num2
+    elif operation == "/":
+        return num1/num2
+    
+    
+
+
---
+++ src/testCalculator.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 19 18:22:59 2020
+
+@author: sanur
+"""
+
+
+import unittest
+from calculator import calculator
+
+class MyTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(5, calculator("+",2,3))
+        self.assertEqual(0, calculator("-",2,2))
+    
+    def test1(self):
+        self.assertEqual(5, calculator("+",2,3))
+        self.assertEqual(3, calculator("-",5,2))
+        
+    def test2(self):
+        self.assertEqual(9, calculator("+",8,1))
+        self.assertEqual(10, calculator("+",7,3))
+        
+if __name__ == '__main__':
+    unittest.main()        
+        
